6b181e360330c2d7db9ad927242fbdcdd9bd5203
Duncan <d.h.ogilvie@gmail.com>
2015-02-24 12:39:27 +0100

    fix

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     var navItems = {
         "Home": "index.html",
         "Best Page Ever!!": "standaard.html",
-        "Reference": "reference.html"
+        "Reference": "reference.html",
+        "Flickr": "flickr.html"
     };
     $.each(navItems, function(title, href) {
         $("#navlist").append("<li><a href=\"" + href + "\">" + title + "</a></li>");
