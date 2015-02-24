08eefa0b29cfc52717ec5d73dc1eab122a438372
Duncan <d.h.ogilvie@gmail.com>
2015-02-24 16:35:26 +0100

    fixed style

--- js/navigation.js
+++ js/navigation.js
@@ -7,10 +7,16 @@ $(document).ready(function() {
     };
     var curPath = $(location).attr("pathname");
     $.each(navItems, function(title, href) {
-        if (!curPath.match(new RegExp(href + "$"))) {
-            $("#navlist").append("<li><a href=\"" + href + "\">" + title + "</a></li>");
-        } else {
+        if (curPath.match(new RegExp(href + "$"))) {
             $("#navlist").append("<li class=\"nolink\">" + title + "</li>");
+        } else {
+            $("#navlist").append("<li><a href=\"" + href + "\">" + title + "</a></li>");
         }
     });
+});
+$(window).scroll(function() {
+    var top = $(window).scrollTop();
+    var fadePos = $(".navigation").position().top;
+    var opacity = top < fadePos ? 1 - (top / fadePos) : 0;
+    $(".title").stop(true, true).fadeTo(0, opacity);
 });
