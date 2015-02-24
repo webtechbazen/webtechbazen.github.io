c86459555d5706da85b3eac3e2ce9e9f2b4eadc6
Duncan <d.h.ogilvie@gmail.com>
2015-02-24 01:12:21 +0100

    added auto-generate references

---
+++ js/reference.js
@@ -0,0 +1,14 @@
+function referenceAdd(refId, url) {
+    $("#references").append("<li><a class=\"reference\" href=\"#" + refId + "\"><b>^</b></a> <a class=\"external\" href=\"" + url + "\">" + url + "</a></li>");
+}
+$(document).ready(function() {
+    var refCount = 1;
+    $("a").each(function() {
+        var href = $(this).attr("href");
+        if (href.charAt(0) != '#') {
+            var refId = "ref-" + refCount++;
+            $(this).prop("id", refId);
+            referenceAdd(refId, href, $(this).text());
+        }
+    });
+});
